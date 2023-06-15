250e839f97e2437ed5afce5fdc6f8df24b9ae7ea
komeylian <komeylian@gmail.com>
2023-06-15 20:34:06 +0330

    O-1402-03-25

--- Ali_01_String_02.py
+++ Ali_01_String_02.py
@@ -1,11 +1,16 @@
 a = str({'BTC': {'USDT': 25728.2}})
 
-
+b = ''
 for i in range(len(a)):
-    if a[i] == '}' or a[i] == '{':
+    if a[i] == '}' or a[i] == '{' or a[i] == "'" or a[i] == ":":
         print(' '.ljust(2), '|', end='')
+
     else:
-        print(a[i].ljust(2), '|', end='')
+        print((a[i]).ljust(2), '|', end='')
+        b += a[i]
 print()
 for ii in range(len(a)):
     print(str(ii).ljust(2), '|', end='')
+
+print("\n")
+print(b)
