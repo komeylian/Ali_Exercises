698fa704659f08a69fdbbbe4ff8589d107d3dafb
komeylian <komeylian@gmail.com>
2023-06-26 10:17:46 +0330

    Array
    Array

---
+++ Ali_01_Array_01.py
@@ -0,0 +1,52 @@
+a = []
+
+print(type(a))
+
+
+a = [1, 2, 13, 4, 5, 6, 7, 8, 9, 0]
+
+
+a[4] = 12
+print(a)
+
+a.append(1415)
+
+print(len(a), a[4])
+print(a)
+
+a.sort()
+# a.reverse()
+a.remove(7)
+
+print(a)
+
+
+for i in range(len(a)):
+    print(i, a[i], end='  |  ')
+    # print()
+    if a[i] == 4:
+        a.insert(i+1, a)
+
+print("\n-------")
+print(a[4][9])
+
+
+for i in range(len(a[4])):
+    print(i, a[4][i])
+
+print('*'*20)
+
+a[4][4] = 10
+
+print(a[4])
+# print(a)
+##########################
+
+
+#a = [['reyazi', '12'], ['olom', '15'], ['dini', '20']]
+# delet in array
+
+'''
+for i in range (0,3):
+    nnn = input "sdDad"
+'''
