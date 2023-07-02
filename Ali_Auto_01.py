fc4bb2be6a46f61a4e285f3a6eee21a061a486fc
komeylian <komeylian@gmail.com>
2023-07-02 17:44:52 +0330

    Update Ali_Auto_01.py

--- Ali_Auto_01.py
+++ Ali_Auto_01.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 import pyautogui
 import webbrowser
 import time
@@ -14,7 +16,7 @@ pyautogui.moveTo(Cusername)
 time.sleep(2)
 pyautogui.click()
 pyautogui.write("k7772")
-time.sleep(3)
+time.sleep(2)
 
 
 # get password
@@ -25,7 +27,7 @@ pyautogui.moveTo(Cpassword)
 time.sleep(2)
 pyautogui.click()
 pyautogui.write("k7772")
-time.sleep(3)
+time.sleep(2)
 
 
 # get enter
@@ -34,34 +36,65 @@ Center = pyautogui.center(enter)
 pyautogui.moveTo(Center)
 time.sleep(1)
 pyautogui.click()
-time.sleep(3)
+time.sleep(2)
 
 
-# get size
-lowersize = pyautogui.locateOnScreen(
-    "C:/Ali_Exercises/lowersize.png", confidence=.8)
-Clowersize = pyautogui.center(lowersize)
-pyautogui.moveTo(Clowersize)
-time.sleep(1)
-pyautogui.click()
-time.sleep(3)
-pyautogui.click()
-time.sleep(3)
-pyautogui.click()
-time.sleep(3)
-pyautogui.click()
-time.sleep(3)
-'''''
+# # Scroll down by 100 units
+# pyautogui.scroll(100)
+# pyautogui.hotkey('page_down')
+# pyautogui.scroll(200, x=100, y=200)
+
+
+# # Scroll down using PAGE_DOWN key
+# pyautogui.hotkey('page_down')
+
+
+# Scroll down using keyboard inputs
+#pyautogui.press('pagedown')
+pyautogui.scroll(-100)
+
+
+
+# # get size
+# lowersize = pyautogui.locateOnScreen(
+#     "C:/Ali_Exercises/lowersize.png", confidence=.8)
+# Clowersize = pyautogui.center(lowersize)
+# pyautogui.moveTo(Clowersize)
+# time.sleep(1)
+# pyautogui.click()
+# time.sleep(3)
+# pyautogui.click()
+# time.sleep(3)
+# pyautogui.click()
+# time.sleep(3)
+# pyautogui.click()
+# time.sleep(3)
+# '''''
 # get lowerbutton
-lowerbutton = pyautogui.locateOnScreen(
-    "C:/Ali_Exercises/lowerbutton.png", confidence=.8)
-Clowerbutton = pyautogui.center(lowerbutton)
-pyautogui.moveTo(Clowerbutton)
-time.sleep(1)
-pyautogui.click()
-time.sleep(3)
-pyautogui.click()
-time.sleep(3)
-pyautogui.click()
-time.sleep(3)
-'''
+# lowerbutton = pyautogui.locateOnScreen(
+#     "C:/Ali_Exercises/lowerbutton.png", confidence=.8)
+# Clowerbutton = pyautogui.center(lowerbutton)
+# pyautogui.moveTo(Clowerbutton)
+# time.sleep(1)
+# pyautogui.click()
+# time.sleep(3)
+# pyautogui.click()
+# time.sleep(3)
+# pyautogui.click()
+# time.sleep(3)
+# '''
+
+
+# # Scroll down by 100 units
+# pyautogui.scroll(100)
+
+
+# # Launch the web browser (e.g., Chrome)
+# driver = webdriver.Chrome()
+
+# # Open a web page
+# driver.get('https://example.com')
+
+# # Scroll down using the body element
+# body = driver.find_element_by_tag_name('body')
+# body.send_keys(Keys.PAGE_DOWN)
